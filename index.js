3d3d6dec10e1d9b9eaaef8ea97121426f14d379e
Kamauleonard <leonardmwangi2012@gmail.com>
2023-10-03 18:25:24 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+// Solution 1
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  console.log(saturdayFun());            
+  
+//   Solution 2
+
+  function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  console.log(mondayWork());            
+  
+  // Solution 3
+  function wrapAdjective(easy = "*") {
+    return function(adjective = "special") {
+      return `You are ${easy}${adjective}${easy}!`;
+    };
+  }
+  
+ 
+  const encouragingPromptFunction = wrapAdjective("!!!");
+ 
+  
